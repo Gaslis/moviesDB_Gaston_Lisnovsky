d2cb75fb6801832d34c698388e164bd12d1e88f2
Gas <gaslis@gmail.com>
2020-01-06 11:34:30 -0300

    footer y head

--- moviesDB/resources/views/partials/footer.blade.php
+++ moviesDB/resources/views/partials/footer.blade.php
@@ -7,22 +7,22 @@
       <a href="/genres/listaGeneros/">Generos</a><br>
     </article>
     <article class="col-12 col-sm-4 col-md-4 mb-5">
-      <h4>Contacto</h4>
+      {{-- <h4>Contacto</h4>
       <a href="mailto: info@graficar.com.ar"><ion-icon name="mail" class="achicarIconos"></ion-icon> contacto@graficar.com</ion-icon></a><br>
        <a href="#"> <ion-icon name="logo-whatsapp" class="achicarIconos"></ion-icon> (011) 15 4054 6753</a><br>
-      <span>Horario: Lunes a Viernes 9 a 18 hs.</span><br>
+      <span>Horario: Lunes a Viernes 9 a 18 hs.</span><br> --}}
     </article>
     <article class="col-12 col-sm-4 col-md-4">
       <h4>Seguinos en:</h4>
 
-      <a href="https://business.facebook.com/graficarmerchandising" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a>
-      <a href="https://www.instagram.com/graficarmerchandising" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+      <a href="" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a>
+      <a href="" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
       <!-- <a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a>
       <a href="#"><ion-icon name="mail"></ion-icon></a> -->
     </article>
   </section>
   <section>
-    <h5 class="fin2 pb-4 pt-4">©Copyright 2019 - Graficar® | Todos los Derechos Reservados </h5>
+    <h5 class="fin2 pb-4 pt-4">©Copyright 2019 - movieDB | Todos los Derechos Reservados </h5>
   </section>
 </footer>
   <script src="https://kit.fontawesome.com/84460efb65.js" crossorigin="anonymous"></script>
