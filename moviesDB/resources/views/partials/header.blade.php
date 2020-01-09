b6ca3b6dd798a48a8732a87d7ad4c30ca989242f
Gas <gaslis@gmail.com>
2020-01-09 11:19:25 -0300

    Update header.blade.php

--- moviesDB/resources/views/partials/header.blade.php
+++ moviesDB/resources/views/partials/header.blade.php
@@ -5,43 +5,39 @@
         {{-- <img src="/img/logo-graficar.png" alt="" class="logo"> --}}
       </a>
     </div>
-  <div class="col-6 col-sm-5 col-md-7 mb-3">
+    <div class="col-6 col-sm-5 col-md-7 mb-3">
     {{-- <form class="pt-3" action="/products/buscarPelicula" method="GET">
       <div class="input-group">
         <input type="text" class="form-control searchRedondo" name="busqueda" value="" placeholder="Encontra la pelicula...">
       </div>
     </form> --}}
-  </div>
-  <div class="dropdown col-6 col-sm-3 col-md-2">
-  <button type="button" class="btn btn-primary dropdown-toggle botonMenu menuMedia" data-toggle="dropdown">
-    ☰
-  </button>
-  <div class="dropdown-menu text-center">
-    @auth
-      <a href="/partials/perfil/{{ Auth::id() }}" >{{Auth::user()->name}}  <ion-icon name="person"></ion-icon></a>
+    </div>
+    <div class="dropdown col-6 col-sm-3 col-md-2">
+      <button type="button" class="btn btn-primary dropdown-toggle botonMenu menuMedia" data-toggle="dropdown">☰
+      </button>
+      <div class="dropdown-menu text-center">
+        @auth
+          <a href="/partials/perfil/{{ Auth::id() }}" >{{Auth::user()->name}}  <ion-icon name="person"></ion-icon></a>
 
-    @else
-      <a href="{{ route('login') }}" class="dropdown-item">Ingresá  <ion-icon name="person"></ion-icon></a>
-
-    @endauth
-    @auth
-
-      <a class="dropdown-item" href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-        {{ __('Logout') }}
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-      </form>
-    @else
-
-      <a href="{{ route('register') }}">Registrate  <ion-icon name="create"></ion-icon></a>
-    @endauth
-    <a href="/movies/listaPeliculas/" class="dropdown-item">Peliculas</a>
-    <a href="/genres/listaGeneros/" class="dropdown-item">Generos</a>
-    <a href="/actors/listaActores/" class="dropdown-item">Actores</a>
-  </div>
+        @else
+          <a href="{{ route('login') }}" class="dropdown-item">Ingresá  <ion-icon name="person"></ion-icon></a>
+        @endauth
+        @auth
+          <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
+        @else
+          <a href="{{ route('register') }}">Registrate  <ion-icon name="create"></ion-icon></a>
+        @endauth
+        <a href="/movies/listaPeliculas/" class="dropdown-item">Peliculas</a>
+        <a href="/genres/listaGeneros/" class="dropdown-item">Generos</a>
+        <a href="/actors/listaActores/" class="dropdown-item">Actores</a>
+      </div>
   {{-- @auth
     <a href="/carrito/index/">
       <button type="submit" class="btn botonChango"><ion-icon name="cart"></ion-icon><span class="badge badge-light">
@@ -56,7 +52,7 @@
         <button type="submit" class="btn botonChango"><ion-icon name="cart"></ion-icon></button>
       </a>
     @endauth --}}
-  </div>
+    </div>
   </nav>
   <div class="row barraRoja py-1">
     <nav class="col-6 primerNav">
@@ -71,20 +67,20 @@
       @endif --}}
     </nav>
     <nav class="col-6 segundoNav">
-        @auth
-          <a href="/partials/perfil/{{ Auth::id() }}">{{Auth::user()->name}}  <ion-icon name="person"></ion-icon></a>
-          <a class="" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-            document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-          </a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-          </form>
-        @else
-          <a href="{{ route('login') }}">Ingresá  <ion-icon name="person"></ion-icon></a>
-          <a href="{{ route('register') }}">Registrate  <ion-icon name="create"></ion-icon></a>
-        @endauth
+      @auth
+        <a href="/partials/perfil/{{ Auth::id() }}">{{Auth::user()->name}}  <ion-icon name="person"></ion-icon></a>
+        <a class="" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+      @else
+        <a href="{{ route('login') }}">Ingresá  <ion-icon name="person"></ion-icon></a>
+        <a href="{{ route('register') }}">Registrate  <ion-icon name="create"></ion-icon></a>
+      @endauth
     </nav>
   </div>
 </header>
